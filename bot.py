d15833eb32c9c8cabd4891b137851e1cdb1ff089
loudtrexx <pelisahko99@gmail.com>
2025-01-15 12:41:30 +0200

    Updated bot.py with comments

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from twitchio.ext import commands
 
-# set up the bot
+# set up the bot by inserting the variables to the .env
 bot = commands.Bot(
     token=os.environ['OAUTH_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
@@ -9,7 +9,7 @@ bot = commands.Bot(
     prefix=os.environ['BOT_PREFIX'],
     initial_channels=[os.environ['CHANNEL']]
 )
-
+# Command to use with prefix of your choice such as ! (!test)
 @bot.command(name='test', aliases=['t'])
 async def test_command(ctx):
     await ctx.send(f'Hello {ctx.author.name}')
@@ -18,9 +18,9 @@ async def test_command(ctx):
 async def test_command2(ctx):
     await ctx.send(f'Oh my')
 
-@bot.command(name='introduce', aliases=['int'])
-async def int_command(ctx):
-    await ctx.send(f'Hello! I am a bot created by @loudtrex to show how skillfull he can be! If you see this massage you are amazing!')
+#@bot.command(name='introduce', aliases=['int'])
+#async def int_command(ctx):
+ #   await ctx.send(f'Hello! I am a bot created by @loudtrex to show how skillfull he can be! If you see this massage you are amazing!')
     
 @bot.command(name='counter', aliases=['count'])
 async def test_command(ctx, user_name: str = None):
