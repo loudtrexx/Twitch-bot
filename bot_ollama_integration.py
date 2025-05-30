f651cfdc3bf37aebc1577daeb77c2e879850bc0e
loudtrexx <pelisahko99@gmail.com>
2025-05-30 08:04:08 +0000

    Add files via upload

--- bot_ollama_integration.py
+++ bot_ollama_integration.py
@@ -1,6 +1,9 @@
 import os
 from twitchio.ext import commands
 from ollama import chat
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # set up the bot by inserting the variables to the .env
@@ -9,9 +12,9 @@ bot = commands.Bot(
     client_id=os.environ['CLIENT_ID'],
     nick=os.environ['BOT_NICK'],
     prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
+    initial_channels=os.getenv('CHANNEL').split(",")
 )
-# Command to use with prefix of your choice such as ! (!test)
+# Command to use with prefix of your choice such as ! (!test) 
 @bot.command(name='test', aliases=['t'])
 async def test_command(ctx):
     await ctx.send(f'Hello {ctx.author.name}')
@@ -22,12 +25,12 @@ async def test_command2(ctx):
 
 @bot.command(name="ask", aliases=["chat"])
 async def chat_command(ctx: commands.Context, *, phrase: str) -> None:
-    Answer = chat(model="gemma2", messages=[
+    Answer = chat(model="gemma3", messages=[
         {"role": "user", "content": phrase}
     ])
     await ctx.send(Answer["message"]["content"][:500])
     print(Answer["message"]["content"])
-    # await ctx.send("Did you find me helpful?")
+    # await ctx.send("Did you find me helpful?") #Debug message, Commented in general use
 
 @bot.command(name="simple")
 async def echo(ctx: commands.Context, *, phrase: str) -> None:
@@ -44,6 +47,28 @@ async def test_command(ctx, user_name: str = None):
         await ctx.send(f"Hello, {user_name}!")
     else:
         await ctx.send("Hello, chat!")
+
+@bot.command(name="check", aliases=["chk", "ck"])
+async def my_command(ctx: commands.Context):
+    channel_name = ctx.channel.name
+    await ctx.send(f"This command was sent in {channel_name}")
+
+@bot.command(name="checkrole", aliases=["rc", "roles"])
+async def check_role(ctx: commands.Context):
+    user = ctx.author
+    if user.is_mod:
+        await ctx.send("Rooted!")
+    else:
+        await ctx.send("NA")
+
+@bot.command(name="dasknfjwknfewkjnf", aliases=["dbg"])
+async def debugroles(ctx: commands.Context, status: bool = None):
+    user = ctx.author
+    if user.name == "loudtrex":
+        await ctx.send("I recognize you!")
+    else:
+        await ctx.send("No, I don't recognize you!")
+
 
 @bot.event
 async def Ready_comm(self):
